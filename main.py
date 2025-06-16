1a6da62c92edffbad1c4c5e091a26708ac4ff687
robbykansas <lomiver@gmail.com>
2025-06-16 11:27:12 +0700

    chore: add cors

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from typing import List
 from contextlib import asynccontextmanager
 from services import RecommendationService
 from qdrant import get_vectordb, init_vectordb
-import os
-
-# os.environ["TOKENIZERS_PARALLELISM"] = "false"
-# os.environ["OMP_NUM_THREADS"] = "1"
-# os.environ["MKL_NUM_THREADS"] = "1"
-# os.environ["OPENBLAS_NUM_THREADS"] = "1"
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 class RecomendationRequest(BaseModel):
     input_text: str
+
+class RecommendationResponse(BaseModel):
+    recommendations: List[dict]
+    error: str = None
 
 recommendation_service = RecommendationService()
 
@@ -23,14 +23,27 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+Origin = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=Origin,
+    allow_methods=["POST"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/recommendations", response_model=List[dict])
+@app.post("/recommendations", response_model=RecommendationResponse)
 async def get_recommendations(request: RecomendationRequest):
     vectordb = get_vectordb()
     try:
-        return recommendation_service.get_recommendations(request.input_text, vectordb)
+        print("Received input text:", request.input_text)
+        res = recommendation_service.get_recommendations(request.input_text, vectordb)
+        return JSONResponse(content=res, media_type="application/json")
     except Exception as e:
         return {"error": str(e)}
