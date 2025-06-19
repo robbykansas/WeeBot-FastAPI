0c086d793196826a37722156968c72c1cf3849c5
robbykansas <lomiver@gmail.com>
2025-06-19 15:26:01 +0700

    feat: use jina token

--- qdrant.py
+++ qdrant.py
@@ -1,5 +1,7 @@
 import os
+from jina import JinaAIEmbedding
 from sentence_transformers import SentenceTransformer
+from langchain_core.embeddings.embeddings import Embeddings
 from langchain_qdrant import Qdrant
 from qdrant_client import QdrantClient
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -8,20 +10,31 @@ load_dotenv()
 
 vectordb: Qdrant | None = None
 
-def is_sentence_transformer_model(path: str) -> bool:
-    required_files = ["config.json", "modules.json"]
-    return all(os.path.isfile(os.path.join(path, f)) for f in required_files)
+class LangchainJinaEmbeddings(Embeddings):
+    def __init__(self, jina_embedder: JinaAIEmbedding):
+        self.jina_embedder = jina_embedder
 
-def load_embedding(local_embedding) -> SentenceTransformer:
-    if is_sentence_transformer_model(local_embedding):
-        print(f"Model found locally at: {local_embedding}")
-        model = SentenceTransformer(local_embedding, trust_remote_code=True, model_kwargs={"default_task": "retrieval.query"})
-    else:
-        print(f"Model not found in {local_embedding}. Downloading from Hugging Face...")
-        model = SentenceTransformer("jinaai/jina-embeddings-v3", trust_remote_code=True, model_kwargs={"default_task": "retrieval.query"})
-        model.save(local_embedding)
+    def embed_documents(self, texts: list[str]) -> list[list[float]]:
+        return [item["embedding"] for item in self.jina_embedder.embed_documents(texts)]
 
-    return local_embedding
+    def embed_query(self, text: str) -> list[float]:
+        return self.jina_embedder.embed_query(text)
+
+
+# def is_sentence_transformer_model(path: str) -> bool:
+#     required_files = ["config.json", "modules.json"]
+#     return all(os.path.isfile(os.path.join(path, f)) for f in required_files)
+
+# def load_embedding(local_embedding) -> SentenceTransformer:
+#     if is_sentence_transformer_model(local_embedding):
+#         print(f"Model found locally at: {local_embedding}")
+#         model = SentenceTransformer(local_embedding, trust_remote_code=True)
+#     else:
+#         print(f"Model not found in {local_embedding}. Downloading from Hugging Face...")
+#         model = SentenceTransformer("jinaai/jina-embeddings-v2-base-en", trust_remote_code=True)
+#         model.save(local_embedding)
+
+#     return local_embedding
 
 async def init_vectordb():
     global vectordb
@@ -34,13 +47,18 @@ async def init_vectordb():
         api_key=os.getenv('QDRANT_API_KEY')
     )
 
-    loc="./hf_embed"
-    os.makedirs(loc, exist_ok=True)
+    # loc="./hf_embed"
+    # os.makedirs(loc, exist_ok=True)
+    jina_api_key = "jina_d0c55237ee3d48e4971f01af99a582b3293BUJC1aiMpOz46tlPs3InYUAij"
+    jina_model = "jina-embeddings-v3"
 
-    embedding_function = HuggingFaceEmbeddings(
-        model_name=load_embedding(loc),
-        model_kwargs={"trust_remote_code": True},
-    )
+    jina_embedder = JinaAIEmbedding(api_key=jina_api_key, model=jina_model)
+    embedding_function = LangchainJinaEmbeddings(jina_embedder)
+
+    # embedding_function = HuggingFaceEmbeddings(
+    #     model_name=load_embedding(loc),
+    #     model_kwargs={"trust_remote_code": True},
+    # )
 
     vectordb = Qdrant(
         client=qdrant,
